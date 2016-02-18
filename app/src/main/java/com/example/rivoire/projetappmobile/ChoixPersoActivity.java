784563a75fab80389c28b83560f89f521d0a09c3
unknown <rivoire@B-0-02-06.iutparis.local>
2016-02-18 16:45:53 +0100

    1er commit projet initial

---
+++ app/src/main/java/com/example/rivoire/projetappmobile/ChoixPersoActivity.java
@@ -0,0 +1,15 @@
+package com.example.rivoire.projetappmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoixPersoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select__character);
+
+
+    }
+}
